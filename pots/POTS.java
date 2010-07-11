78594ffbcd50cb0e21f29b2500bc76718daa8547
Rafał Rusin <rafal@intalio.com>
2010-07-11 14:13:28 +0200

    Saving dumps to file using JSON format

--- pots/POTS.java
+++ pots/POTS.java
@@ -1,38 +1,112 @@
 package pots;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class POTS {
     public static class Dump {
+        private List<Object> tokens = new ArrayList<Object>();
+        
+        public static class ArgToken {
+            private Object obj;
+
+            public ArgToken(Object obj) {
+                this.obj = obj;
+            }
+            
+            private static String fetchValue(Object obj) {
+                if (obj instanceof String) {
+                    return (String) obj;
+                } else return "unknown";
+            }
+            
+            public String toString() {
+                if (obj == null) return "argument: {},";
+                else return "argument: {"
+                + "class: " + quote(obj.getClass().getCanonicalName())
+                + ", value: " + quote(fetchValue(obj))
+                + "},";
+            }
+        }
+        
+        public void init() {
+            tokens.add("threads:[");
+        }
+        
+        private static String quote(String s) {
+            return "\"" + s + "\"";
+        }
+        
         public void visitThread(Thread t, int state, long cpuTime) {
             System.out.println("jthread: " + t.getName() + " state:" + state + " cpuTime:" + cpuTime);
+            tokens.add("thread:{");
+            tokens.add("name:" + quote(t.getName()));
+            tokens.add(", state:" + quote("" + state));
+            tokens.add(", cpuTime:" + quote("" + cpuTime));
+            tokens.add(", frames:[");
         }
 
         public void visitMethod(Class methodClass, String methodName, int location) {
             System.out.println("jmethod: " + methodClass + " " + methodName+ " line:" + location);
+            tokens.add("frame: {");
+            tokens.add("methodClass: " + quote(methodClass.toString()));
+            tokens.add(", methodName: " + quote(methodName.toString()));
+            tokens.add(", location: " + quote("" + location));
+            tokens.add(", args: [");
         }
         public void visitArg(Object arg) {
-            String argClass = arg != null ? arg.getClass().toString() : "";
-            System.out.println("jarg: " + argClass);
+            tokens.add(new ArgToken(arg));
         }
         public void visitArg(int arg) {
             System.out.println("jarg: " + arg);
+            tokens.add(new ArgToken(arg));
         }
         public void visitArg(long arg) {
             System.out.println("jarg: " + arg);
+            tokens.add(new ArgToken(arg));
         }
         public void visitArg(float arg) {
             System.out.println("jarg: " + arg);
+            tokens.add(new ArgToken(arg));
         }
         public void visitArg(double arg) {
             System.out.println("jarg: " + arg);
+            tokens.add(new ArgToken(arg));
         }
         public void visitMethodEnd() {
             System.out.println("jmethod end");
+            tokens.add("]},");
         }
         public void visitThreadEnd() {
             System.out.println("jthread end");
+            tokens.add("]},");
         }
 
         public void completed() {
+            tokens.add("]");
             System.out.println("jcompleted");
+            
+            saveTokens("pots.log");
+        }
+        
+        private void saveTokens(String fileName) {
+            StringBuffer buf = new StringBuffer();
+            for (Object o : tokens) {
+                buf.append(o.toString());
+            }
+            buf.append("\n");
+            try {
+                OutputStream out = new FileOutputStream(fileName, true);
+                PrintWriter wt = new PrintWriter(out);
+                wt.write(buf.toString());
+                wt.close();
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -75,6 +149,8 @@ public class POTS {
     
     public static Dump newDump() {
         System.out.println("init");
-        return new Dump();
+        Dump d = new Dump();
+        d.init();
+        return d;
     }
 }
